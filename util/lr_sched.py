864e3abe01caed05471156551205d1946a565eca
Hyejun Shin <92062760+HyejunShin@users.noreply.github.com>
2023-12-12 08:50:08 -0500

    Update lr_sched.py

--- util/lr_sched.py
+++ util/lr_sched.py
@@ -19,18 +19,33 @@ def adjust_learning_rate(optimizer, epoch, args):
         else:
             param_group["lr"] = lr
     return lr
-#
-# def adjust_learning_rate_v2(optimizer, steps, args,step_per_epoch):
-#     """Decay the learning rate with half-cycle cosine after warmup"""
-#     if steps < args.warmup_epochs:
-#         lr = args.lr * steps / (args.warmup_epochs *step_per_epoch)
-#     else:
-#         lr = args.min_lr + (args.lr - args.min_lr) * 0.5 * \
-#             (1. + math.cos(math.pi * (steps - args.warmup_epochs*step_per_epoch) / (args.epochs*step_per_epoch - step_per_epoch*args.warmup_epochs)))
-#     for param_group in optimizer.param_groups:
-#         if "lr_scale" in param_group:
-#             param_group["lr"] = lr * param_group["lr_scale"]
-#         else:
-#             param_group["lr"] = lr
-#     return lr
+
+def adjust_learning_rate_triangular2(optimizer, epoch, args):
+    """Decay the learning rate with triangular2"""
+    num_cycles = 5
+    cycle_length = args.epochs / num_cycles
+    cycle = math.floor(1 + epoch / (2 * cycle_length))
+    x = abs(epoch / cycle_length - 2 * cycle + 1)
+    lr = args.min_lr + (args.lr - args.min_lr) * max(0, (1 - x)) / (2 ** (cycle - 1))
+    for param_group in optimizer.param_groups:
+        if "lr_scale" in param_group:
+            param_group["lr"] = lr * param_group["lr_scale"]
+        else:
+            param_group["lr"] = lr
+    return lr
+
+def adjust_learning_rate_step_decay(optimizer, epoch, args):
+    """Decay the learning rate with step decay after warmup"""
+    if epoch < args.warmup_epochs:
+        lr = args.lr * epoch / args.warmup_epochs
+    else:
+        decay_factor = 0.65
+        step_size = 2
+        lr = args.lr * (decay_factor ** ((epoch - args.warmup_epochs) // step_size))
+    for param_group in optimizer.param_groups:
+        if "lr_scale" in param_group:
+            param_group["lr"] = lr * param_group["lr_scale"]
+        else:
+            param_group["lr"] = lr
+    return lr
 
